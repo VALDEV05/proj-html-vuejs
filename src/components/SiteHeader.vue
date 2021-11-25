0fe1d35b5699958c170f5035cccf87e3bd20734c
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-11-25 14:38:25 +0100

    Complete Jumbotron

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -7,16 +7,16 @@
             </div>
             <div id="menu_header">
                 <ul class="list-unstyled d-flex mt-3">
-                    <li class="px-3"><a class="text-uppercase text-decoration-none" href="#">home</a></li>
-                    <li class="px-3"><a class="text-uppercase text-decoration-none" href="#">pages</a></li>
-                    <li class="px-3"><a class="text-uppercase text-decoration-none" href="#">program</a></li>
-                    <li class="px-3"><a class="text-uppercase text-decoration-none" href="#">tickets</a></li>
-                    <li class="px-3"><a class="text-uppercase text-decoration-none" href="#">speakers</a></li>
-                    <li class="px-3"><a class="text-uppercase text-decoration-none" href="#">papers</a></li>
-                    <li class="px-3"><a class="text-uppercase text-decoration-none" href="#">blog</a></li>
-                    <li class="px-3"><a class="text-uppercase text-decoration-none" href="#">shortcodes</a></li>
+                    <li class="px-3"><a class="text-uppercase link_header text-decoration-none" href="#">home</a></li>
+                    <li class="px-3"><a class="text-uppercase link_header text-decoration-none" href="#">pages</a></li>
+                    <li class="px-3"><a class="text-uppercase link_header text-decoration-none" href="#">program</a></li>
+                    <li class="px-3"><a class="text-uppercase link_header text-decoration-none" href="#">tickets</a></li>
+                    <li class="px-3"><a class="text-uppercase link_header text-decoration-none" href="#">speakers</a></li>
+                    <li class="px-3"><a class="text-uppercase link_header text-decoration-none" href="#">papers</a></li>
+                    <li class="px-3"><a class="text-uppercase link_header text-decoration-none" href="#">blog</a></li>
+                    <li class="px-3"><a class="text-uppercase link_header text-decoration-none" href="#">shortcodes</a></li>
                     <li id="separate" class="px-3">|</li>
-                    <li class="px-3"><a class="text-uppercase text-decoration-none" href="#"><img src="@/assets/img/search-solid.svg" alt=""></a></li>
+                    <li class="px-3"><a class="text-uppercase link_header text-decoration-none" href="#"><img src="@/assets/img/search-solid.svg" alt=""></a></li>
                 </ul>
             </div>
 
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,8 +1,26 @@
 <template>
-  <div id="siteMain">
-      <div class="container">
-        <h1>{{title}}</h1>   
-      </div>
+  <div id="siteMain"> 
+      <div id="jumbotron" class="d-flex align-items-center"> 
+          <div class="container text-center text-uppercase">
+              <div class="title text-light">
+                  <p  class="m-0 title-sm">Conference on</p>
+                  <p class="m-0 title-lg fw-bold">social networks</p>
+              </div> 
+              <img src="@/assets/img/1-item-2.jpg" alt="">
+              <div id="info" class="d-flex flex-row justify-content-center">
+                  <ul class="list-unstyled list-inline">
+                      <li class=" list-inline-item"><a class="text-light fw-bold text-uppercase text-decoration-none" href="#">23 - 26 MAY 2019 </a></li>
+                      <li class="px-4 list-inline-item"><a class="text-light fw-bold text-uppercase text-decoration-none" href="#"> / </a></li>
+                      <li class=" list-inline-item"><a class="text-light fw-bold text-uppercase text-decoration-none" href="#"> SANS FRANCISCO </a></li>
+                      <li class="px-4 list-inline-item"><a class="text-light fw-bold text-uppercase text-decoration-none" href="#"> / </a></li>
+                      <li class="list-inline-item"><a class="text-light fw-bold text-uppercase text-decoration-none" href="#"> 8 SPEAKERS </a></li>
+                  </ul>
+              </div>
+              <div class="register">
+                  <button type="button" class="mt-4 btn btn_register text-uppercase">register online</button>
+              </div>
+          </div>
+    </div>
   </div>
   <!-- /siteMain -->
 </template>
