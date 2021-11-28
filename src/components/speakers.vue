c27a1495e86d419850bf2cdf3acc738bc6641bf4
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-11-28 15:21:53 +0100

    Speakers dinamically

--- src/components/speakers.vue
+++ src/components/speakers.vue
@@ -13,12 +13,26 @@
             <!-- /controls -->
             <div class="section_card_speakers">
                 <div class="row mt-5">
-                    <div class="col-3" v-for="card in cards" :key="card.name">
+                    <div class="col-3 container_card" v-for="card in cards" :key="card.name">
                         <div class="card border-0" style="width: 18rem;">
-                            <img :src="require('@/assets/img/' + card.image)" class="card-img-top" alt="speaker-6">
+                            <img :src="require('@/assets/img/' + card.image)" class="card-img-top image" alt="speaker-6">
                             <div class="text_card text-center pt-3">
                                 <p class="m-0 title-card text-uppercase">{{card.name}}</p>
                                 <p class="m-0 subtitle-card">{{card.role}}</p>
+                            </div>
+                            <div class="middle">
+                                <div class="text text-light d-flex">
+                                    <div class="square mx-1">
+                                        <img src="@/assets/img/twitter-brands-white.svg"  alt="">
+                                    </div>
+                                    <div class="square mx-1">
+                                         <img src="@/assets/img/facebook-f-brands-white-h70.svg" alt="">
+                                    </div>
+                                    <div class="square mx-1">
+                                         <img src="@/assets/img/linkedin-in-brands-white.svg" alt="">
+
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <!-- /card -->
@@ -67,17 +81,14 @@ export default {
     }
 }
 </script>
-
 <style lang="scss">
-@import '@/assets/style/variables.scss';
-
 #speakers{
     background-color: #F3F3F3;
     height: 500px;
     .controls{
         padding-top: 50px;
         .btn_angle{
-        background-color: $scarlet-red-color;   
+        background-color: red;   
         height: 30px;
         width: 30px;
         padding: 20px;
@@ -85,7 +96,7 @@ export default {
         display: flex;
         align-items: center;
         justify-content: center;
-        color: $white-color;
+        color: white;
         font-weight: bold;
     
         }
@@ -101,11 +112,44 @@ export default {
                 }
             
             .subtitle-card{
-                font-family: 'Bitter', serif;
-                color: $icon-gray;
+                color: gray;
             }
         }
 }
+.container_card {
+  position: relative;
+  width: 50%;
+}
 
+.container_card:hover .middle {
+    opacity: 0.6;
+}
 
+.text {
+  font-size: 16px;
+  padding: 16px 32px;
+}
+.middle {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 209px;
+    width: 18rem;
+    opacity: 0.0;
+    transition: .5s ease;
+     position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -75%);
+    text-align: center;
+    background-color: red;
+}
+.square{
+    height: 70px;
+    width: 70px;
+    border: 3px solid white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 </style>
