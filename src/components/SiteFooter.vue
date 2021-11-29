73f518e3b0083e9bacf266bdad4235e8abf2a6eb
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-11-29 15:41:23 +0100

    Footer totally dinamically

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -3,47 +3,31 @@
       <div class="container">
         <div class="row text-light">
           <div class="col-3 mt-5">
-            <h1 class="text-uppercase">about us</h1>
-            <p class="paragraph">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+            <h1 class="text-uppercase">{{titleFirstCol}}</h1>
+            <p class="paragraph">{{subtitleFirstCol}}</p>
             <div class="row social">
-              <div class="col-2">
-                <div class="square">
-                  <a href="#"></a>
+              <div class="col-2" v-for="icon in iconSocial" :key="icon.icon">
+                <div class="square d-flex justify-content-center align-items-center">
+                  <a :href="icon.iconLink"><img :src="require('@/assets/img/' + icon.iconUrl)" alt=""></a>
                 </div>
-              </div>
-              <div class="col-2">
-                <div class="square"></div>
-              </div>
-              <div class="col-2">
-                <div class="square"></div>
-              </div>
-              <div class="col-2">
-                <div class="square"></div>
-              </div>
-              <div class="col-2">
-                <div class="square"></div>
-              </div>
-              <div class="col-2">
-                <div class="square"></div>
               </div>
 
             </div>
           </div>
           <div class="col-3 mt-5">
-            <h1 class="text-uppercase">Recent news</h1>
-            <p><a class="text-decoration-none text-light text-uppercase" href="#">&gt; &emsp14; Sem porta mollis parturient</a></p>
-            <p><a class="text-decoration-none text-light text-uppercase" href="#">&gt; &emsp14; Sem porta mollis parturient</a></p>
-            <p><a class="text-decoration-none text-light text-uppercase" href="#">&gt; &emsp14; Sem porta mollis parturient</a></p>
-            <p><a class="text-decoration-none text-light text-uppercase" href="#">&gt; &emsp14; Sem porta mollis parturient</a></p>
-            <p><a class="text-decoration-none text-light text-uppercase" href="#">&gt; &emsp14; Sem porta mollis parturient</a></p>
+            <h1 class="text-uppercase">{{titleSecondCol}}</h1>
+            <div class="recentNews" v-for="news in recentNews" :key="news.news">
+                <p><a class="text-decoration-none text-light text-uppercase text-muted" href="#">&gt; &emsp14; {{news.news}}</a></p>
+            </div>
+            
           </div>
           <div class="col-3 mt-5" >
-            <h1 class="text-uppercase">get your seat!</h1>
-            <p>Book for your seat before it's too rate. we provide variety of ticket options</p>
-            <div class="checkBtn mt-4 text-uppercase text-center fw-bold"><a href="#" class="text-light text-decoration-none">check out tickets!</a> </div>
+            <h1 class="text-uppercase">{{titleThirdCol}}</h1>
+            <p>{{subtitleThirdCol}}</p>
+            <div class="checkBtn mt-4 text-uppercase text-center fw-bold"><a href="#" class="text-light text-decoration-none">{{checkThirdCol}}</a> </div>
           </div>
           <div class="col-3 mt-5 gallery">
-            <h1 class="text-uppercase">{{titleGallery}}</h1>
+            <h1 class="text-uppercase">{{titleFourCol}}</h1>
             <div class="row">
               <div class="col-3 m-0" v-for="img in gallery" :key="img.img">
                 <img :src="require('@/assets/img/' + img.imageUrl)" alt="">
@@ -62,7 +46,48 @@ export default {
 
   data(){
     return{
-      titleGallery:'gallery from last year',
+      titleFirstCol:'About us',
+      subtitleFirstCol:'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+      titleSecondCol:'Recent news',
+      titleThirdCol:'get your seat!',
+      subtitleThirdCol: "Book for your seat before it's too rate. we provide variety of ticket options",
+      checkThirdCol:'check out tickets!',
+      titleFourCol:'gallery from last year',
+      iconSocial:[
+        {
+          iconUrl:'icon-square/facebook-square-brands.svg',
+          iconLink:'https://www.facebook.com/'
+        },
+        {
+          iconUrl:'icon-square/twitter-square-brands.svg',
+          iconLink:'https://twitter.com/?lang=it'
+        },
+        {
+          iconUrl:'icon-square/linkedin-brands.svg',
+          iconLink:'https://www.linkedin.com/'
+        },
+        {
+          iconUrl:'icon-square/google-plus-square-brands.svg',
+          iconLink:'https://it.wikipedia.org/wiki/Google%2B/'
+        },
+        {
+          iconUrl:'icon-square/pinterest-square-brands.svg',
+          iconLink:'https://www.pinterest.it/'
+        },
+        {
+          iconUrl:'icon-square/double-dot-square.svg',
+          iconLink:'#'
+        },
+
+      ],
+      recentNews:[
+        {news:'Sem porta mollis parturient'},
+        {news:'nullam lorem mattis purus'},
+        {news:'nibm sem sit ullamcorporer'},
+        {news:'donec luctus imperdiet'},
+        {news:'magna pars studiorum'},
+      ],
+
       gallery:[
         {imageUrl:'photodune-7770665-two-white-coffee-mug-with-diy-decoration-o (1).jpg'},
         {imageUrl:'photodune-8523683-speaker-at-business-convention-and-present.jpg'},
@@ -111,10 +136,12 @@ export default {
     width: 85%;
   }
   .square{
-    background-color: white;
     height: 35px;
     width: 35px;
     border-radius: 5px;
+    img{
+      filter: invert(1);
+    }
   }
 }
 
