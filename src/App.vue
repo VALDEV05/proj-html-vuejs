6ec87e28b0c40fe49a03e3d296010ee5348d057c
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-11-29 15:54:07 +0100

    Add animations jumbotron social

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  </head>
   <div id="app">
     <SiteHeader :items="elementsNavbar"/>
     <SiteMain />
     <SiteFooter title="Footer"/>
 
   </div>
+</html>
+
 </template>
 
 <script>
--- src/components/jumbotronSocialNetwork.vue
+++ src/components/jumbotronSocialNetwork.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="container-jumbo text-center text-uppercase pt-3">
         <div class="title text-light">
-            <p  class="m-0 title-sm">{{title}}</p>
-            <p class="m-0 title-lg fw-bold">{{titleLG}}</p>
+            <p  class="m-0 title-sm animated bounceInDown">{{title}}</p>
+            <p class="m-0 title-lg fw-bold animated zoomIn">{{titleLG}}</p>
         </div> 
-        <img src="@/assets/img/1-item-2.jpg" alt="">
+        <img src="@/assets/img/1-item-2.jpg" class="animated bounceInDown" alt="">
         <div id="info" class="d-flex flex-row justify-content-center">
-            <ul class="list-unstyled list-inline">
-                <li class="px-3 list-inline-item" v-for="item in items" :key="item.data"><a class="text-light fw-bold text-uppercase text-decoration-none" href="#">{{item.info}}</a></li>
+            <ul class="list-unstyled list-inline animated bounceInDown">
+                <li class="px-3 list-inline-item " v-for="item in items" :key="item.data"><a class="text-light fw-bold text-uppercase text-decoration-none" href="#">{{item.info}}</a></li>
             </ul>
         </div>
         <div class="register">
-            <button type="button" class="btn btn_register text-uppercase">{{buttonText}}</button>
+            <button type="button" class="btn btn_register text-uppercase animated bounceInDown">{{buttonText}}</button>
         </div>
     </div>
 </template>
@@ -41,10 +41,20 @@ props:{
         height: 50%;
         .title-sm {
             font-size: 50px;
+            &.animated{
+                animation-duration: 3s;
+            }
         }
         .title-lg {
             font-size: 90px;
             margin-top: -16px;
+            &.animated{
+                animation-duration: 6s;
+            }
+        }
+        .animated.bounceInDown{
+            animation-duration: 3s;
+            
         }
         .btn_register {
             margin-top: 30px !important;
